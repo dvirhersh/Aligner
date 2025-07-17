14f36b79585ed7de9bd87c9e1d4151d23323b23b
Dvir Hershkovits <dvirhersh@gmail.com>
2025-07-17 11:23:31 +0300

    Environment Coding
    Signed-off-by: Dvir Hershkovits <dvirhersh@gmail.com>

---
+++ verif/testbench.sv
@@ -0,0 +1,40 @@
+`include "tests/cfs_algn_test_pkg.sv"
+
+module testbench ();
+
+    import uvm_pkg::*;
+    import cfs_algn_test_pkg::*;
+
+    reg clk;
+
+    initial begin
+        clk = 0;
+        forever begin
+            clk = #5ns ~clk;
+        end
+    end
+
+    reg reset_n;
+
+    initial begin
+        reset_n = 1;
+        #6ns;
+        reset_n = 0;
+        #30ns;
+        reset_n = 1;
+    end
+
+    initial begin
+
+        $dumpfile("dump.vcd");
+        $dumpvars;
+
+        run_test("cfs_algn_test_reg_access");
+    end
+
+    cfs_aligner dut(
+        .clk(    clk),
+        .reset_n(reset_n)
+    );
+
+endmodule
